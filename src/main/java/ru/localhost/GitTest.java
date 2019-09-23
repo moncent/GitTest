80b6a5b5eecc1f42e80e6e235f0da89ac88dc288
Alexander <m.mail777@yandex.ru>
2019-09-23 23:29:26 +0300

    Step 1

--- src/main/java/ru/localhost/GitTest.java
+++ src/main/java/ru/localhost/GitTest.java
@@ -2,6 +2,7 @@ package ru.localhost;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("This is Git Test!");
+        System.out.println("This is Git!");
+        System.out.println("This is Test...");
     }
 }
