1e0f35554df785344ad409fabe7cabf58ce81621
Alexander <m.mail777@yandex.ru>
2022-12-24 00:04:23 +0300

    commit #2

--- src/main/java/ru/localhost/GitTest.java
+++ src/main/java/ru/localhost/GitTest.java
@@ -3,6 +3,7 @@ package ru.localhost;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("This is Git!");
+        System.out.println("Smthing");
         System.out.println("This is Test...");
     }
 }
